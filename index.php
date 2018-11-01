9fd0d169149adc77c243823d1cfc7ca2571d1447
officialmaj <sa646@njit.edu>
2018-11-01 16:12:39 -0400

    changed names of files to proper name extensions

---
+++ index.php
@@ -0,0 +1,74 @@
+<DOCTYPE! html>
+
+	<head> 
+		
+		<title>
+		
+			PROJECT 1
+
+		</title>
+
+	</head>
+
+	<body>
+
+		<form id="loginForm">
+
+			<input type="email" placeholder="Email">
+			<br>
+			<input type="password" placeholder="Password">
+
+
+
+		</form>
+
+		<form method="post" action=""> 
+			
+			<div class="form-field">
+				<label for="fname"> First Name </label>
+				<input type="text" id="fname" 
+				placeholder="Enter your first name">
+			</div>
+
+			<div class="form-field">
+				<label for="lname"> Last Name </label>
+				<input type="text" id="lname"
+				placeholder="Enter your last name">
+			</div>
+			<div class="form-field">
+				<label for="birthday"> Birthday </label>
+				<input type="date" id="birthday"
+				placeholder="Enter your birthday">
+			</div>
+			<div class="form-field">
+				<label for="email"> Email </label>
+				<input type="email" id="email"
+				placeholder="Enter yyour email address">
+			</div>
+
+
+
+
+
+
+
+
+
+
+
+			
+		</form>
+
+	</body>
+
+
+
+
+
+
+
+
+
+
+
+</html>
