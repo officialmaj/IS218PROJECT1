0a0ab1d1e39b7117562b272a7f1c6de93d097ccc
officialmaj <sa646@njit.edu>
2018-11-08 13:01:34 -0500

    made css corrections to place first required if login validation is not correct

--- loginForm.php
+++ loginForm.php
@@ -16,22 +16,43 @@
 	</head>
 
 	<body>
-
+		
 		<form id="loginForm" method="post" action="<?php $_PHP_SELF ?>">
 
 
-			<input id="email" type="email" name="email" placeholder="Email" value="<?php echo $email?>">
+			<input id="email" type="email" name="email"
+			placeholder="Email">
 
+				
 				<?php 
 				
 					$email = $_POST['email'];
-					echo($email);
+					
+					if($email == "")
+					{
+
+						echo "<p style =
+						\"display:inline\">" . " required" . "<p>";
+						echo "<style>" . 
+						
+							"p 
+							{
+							
+								color: red;
+
+							}"
+						
+						    . "</style>";
+
+					}
+
+
 
 				?>
-
+				
 			<br>
 			<input id="password" type="password" name="password" placeholder="Password" value="<?php echo $password ?>">
-
+			<br> <br>
 			<input id="submit" type="submit" name="submit"
 
 							
