178bd5d2e74a77693a2a63318a26e67a0e495087
officialmaj <sa646@njit.edu>
2018-11-28 14:57:53 -0500

    added each of the form fields (question name, question body, question skills)

--- questionForm.php
+++ questionForm.php
@@ -1,27 +1,22 @@
 <html>
-
-
 	<head>
-
-		<title> </title> 
-
+		<title> Question Form  </title> 
 	</head>
 
+	<body>
+		<form id="questionForm" method="post" action="<?php echo $_PHP_SELF?>">	
+		
+		<label name=="questionName"> Question Name </label>
+		<input id="questionName" type="text" name="questionName" placeholder="Enter question name">
+		<br>
+		<label name="questionBody"> Question Body </label>
+		<input id="questionBody" type="text" name="questionBody" placeholder="Enter the question">	
+		<br>
+		<label name="questionSkills"> Question Skills </label>
+		<input id="questionSkills" type="text" name="questionSkills" placeholder="Enter question skill">
+		
+		</form>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+			
+	</body>
 </html>
